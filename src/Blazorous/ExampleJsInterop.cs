5a0330d79296b8d53c437b54b106924de50398d8
Chanan Braunstein <chanan@bkbunch.com>
2018-05-13 16:07:54 -0600

    Add project files.

---
+++ src/Blazorous/ExampleJsInterop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace Blazorous
+{
+    public class ExampleJsInterop
+    {
+        public static string Prompt(string message)
+        {
+            return RegisteredFunction.Invoke<string>(
+                "Blazorous.ExampleJsInterop.Prompt",
+                message);
+        }
+    }
+}
